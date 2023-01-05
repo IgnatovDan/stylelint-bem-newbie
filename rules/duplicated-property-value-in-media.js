7b56d779578a12eedffaf952b40c4b91622d1a98
Ignatov Dan <ignatovdan@gmail.com>
2023-01-06 01:26:03 +0300

    Add duplicated-property-value-in-media

---
+++ rules/duplicated-property-value-in-media.js
@@ -0,0 +1,69 @@
+const stylelint = require('stylelint');
+const { pluginNamespace } = require('./utils/plugin-namespace');
+const { unknownErrorOccurredRuleMessage } = require('./utils/unknownErrorOccurredRuleMessage');
+
+const { report, ruleMessages } = stylelint.utils;
+const ruleName = `${pluginNamespace}/duplicated-property-value-in-media`;
+
+const messages = ruleMessages(ruleName, {
+  unexpectedDuplicatedPropertyValue:
+    (propertyDeclaration, firstMedia, secondaryMedia) =>
+      // eslint-disable-next-line implicit-arrow-linebreak
+      `Unexpected '${propertyDeclaration}' in '${secondaryMedia}' duplicates the same declaration in '${firstMedia}'`,
+  unknownErrorOccurred: unknownErrorOccurredRuleMessage,
+});
+
+function getRuleDisplayName(rule) {
+  // https://postcss.org/api/
+  // Rule#type
+  // Possible values are root, atrule, rule, decl, or comment.
+  if (rule.parent?.type === 'root') {
+    return rule.selector;
+  }
+  if (rule.parent?.type === 'atrule') {
+    return `@${rule.parent?.name} ${rule.parent?.params}`;
+  }
+  /* istanbul ignore next */
+  return 'unknown';
+}
+
+function isSecondaryDeclaration(declarations, decl) {
+  return (decl.prop in declarations && declarations[decl.prop]
+    && (declarations[decl.prop].parent?.selector === decl.parent?.selector));
+}
+
+const ruleFunction = () => (root, result) => {
+  const declarations = {};
+  // https://postcss.org/api/
+  root.walkDecls((decl) => {
+    try {
+      if (isSecondaryDeclaration(declarations, decl)) {
+        if (declarations[decl.prop].value === decl.value) {
+          const firstParentDisplayText = getRuleDisplayName(declarations[decl.prop].parent);
+          const secondParentDisplayText = getRuleDisplayName(decl.parent);
+          // https://postcss.org/api/
+          // Declaration#toString()
+          const declDisplayText = decl.toString();
+
+          report({
+            ruleName,
+            result,
+            message: messages.unexpectedDuplicatedPropertyValue(declDisplayText, firstParentDisplayText, secondParentDisplayText),
+            node: decl,
+          });
+        }
+      }
+      declarations[decl.prop] = decl;
+    } catch (e) {
+      /* istanbul ignore next */
+      report({
+        ruleName, result, message: messages.unknownErrorOccurred(e), node: decl,
+      });
+    }
+  });
+};
+
+ruleFunction.ruleName = ruleName;
+ruleFunction.messages = messages;
+
+module.exports = stylelint.createPlugin(ruleName, ruleFunction);
