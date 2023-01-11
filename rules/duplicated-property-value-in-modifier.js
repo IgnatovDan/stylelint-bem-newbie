4202992857d3aacac2499cc0edeebec5e9cf197a
Ignatov Dan <ignatovdan@gmail.com>
2023-01-11 22:26:00 +0300

    Add rule/tests

--- rules/duplicated-property-value-in-modifier.js
+++ rules/duplicated-property-value-in-modifier.js
@@ -1,23 +1,9 @@
-/*
-
-  - parse string into groups based on regex from https://github.com/IgnatovDan/bem-project/blob/main/.stylelintrc.js
-    - https://javascript.info/regexp-groups#named-groups
-    "^[a-z]+[a-z\\-]*(__[a-z]+[a-z\\-]*)?(_[a-z]+[a-z\\-]*){0,2}$"
-    new String('block-block__el_mod_modvalue').match('(?<block>^[a-z]+[a-z\\-]*)(__(?<el>[a-z]+[a-z\\-]*))?(_(?<mod>[a-z]+[a-z\\-]*))(_(?<modvalue>[a-z]+[a-z\\-]*))$')
-    groups :  {block: 'block-block', el: 'el', mod: 'mod', modvalue: 'modvalue'}
-
-  - Other links:
-    - https://github.com/bem/bem-sdk
-    - https://github.com/bem/bem-sdk/blob/master/packages/naming.entity.parse/index.js
-    - https://github.com/bem-sdk-archive/bem-naming/blob/master/lib/create-parse.js
-    - https://github.com/nglazov/bem-validator-page/blob/main/src/index.js
-
-*/
-
 const stylelint = require('stylelint');
 const fs = require('fs');
+const path = require('path');
 const { pluginNamespace } = require('./utils/plugin-namespace');
 const { unknownErrorOccurredRuleMessage } = require('./utils/unknownErrorOccurredRuleMessage');
+const { tryParseBemName } = require('./utils/try-parse-bem-name');
 
 const { report, ruleMessages } = stylelint.utils;
 const ruleName = `${pluginNamespace}/duplicated-property-value-in-modifier`;
@@ -31,15 +17,35 @@ const messages = ruleMessages(ruleName, {
 });
 
 const ruleFunction = () => (root, result) => {
-  const ff = fs.readFileSync('c:\\c.cc');
+  const cssFullFilePath = root.source?.input?.file;
+  const { name: fileName, dir: fileDir } = path.parse(cssFullFilePath);
+
+  const bemName = tryParseBemName(fileName);
+  if (!bemName) {
+    return;
+  }
+
+  let modOwnerFileBase = bemName.block;
+  if (bemName.el) {
+    modOwnerFileBase += `__${bemName.el}`;
+  }
+  const modOwnerFullFileName = `${fileDir}/../${modOwnerFileBase}.css`;
+  console.log(`modOwnerFullFileName: ${modOwnerFullFileName}`);
+  if (!fs.existsSync(modOwnerFullFileName)) {
+    console.log('not exists');
+    return;
+  }
+
+  const ff = fs.readFileSync(modOwnerFullFileName);
+  console.log('ff');
   console.log(ff);
-  return;
-  report({
-    ruleName,
-    result,
-    message: messages.unexpectedDuplicatedPropertyValue('sadf', 'sadf', 'sdf'),
-    node: root
-  });
+
+  // report({
+  //   ruleName,
+  //   result,
+  //   message: messages.unexpectedDuplicatedPropertyValue('sadf', 'sadf', 'sdf'),
+  //   node: root
+  // });
 };
 
 ruleFunction.ruleName = ruleName;
--- tests/duplicated-property-value-in-modifier.test.js
+++ tests/duplicated-property-value-in-modifier.test.js
@@ -2,23 +2,27 @@ const getTestRule = require('jest-preset-stylelint/getTestRule');
 const fs = require('fs');
 const { ruleName, rule } = require('../rules/duplicated-property-value-in-modifier');
 
-
 const testRule = getTestRule({ plugins: ['./rules/duplicated-property-value-in-modifier.js'] });
 const { messages } = rule;
 
 const { readFileSync } = fs;
+const widthZeroName = 'width-zero';
 
+// eslint-disable-next-line no-undef
 beforeEach(() => {
+  // eslint-disable-next-line no-undef
   jest.spyOn(fs, 'readFileSync').mockImplementation((path, options) => {
-    if (path.toString().includes('c:\\c.cc')) {
-      return '42';
+    if (path.toString().toLowerCase().includes(`${widthZeroName}.css`)) {
+      return `.${widthZeroName} { width: 0; }`;
     }
 
     return readFileSync(path, options);
   });
 });
 
+// eslint-disable-next-line no-undef
 afterEach(() => {
+  // eslint-disable-next-line no-undef
   jest.clearAllMocks();
 });
 
@@ -41,15 +45,37 @@ testRule({
   skipBasicChecks: true,
   codeFilename: 'blocks/page.css',
   accept: [
-    { code: '.page_active { width: 0; }' },
+    { code: '.page { width: 0; }', description: 'blocks/page.css + .page' },
+  ],
+});
+
+testRule({
+  ruleName,
+  config: true,
+  skipBasicChecks: true,
+  codeFilename: 'blocks/page_owner-file-does-not-exist.css',
+  accept: [
+    { code: '.page_owner-file-does-not-exist { width: 0; }', description: 'blocks/page_owner-file-does-not-exist.css' },
+  ],
+});
+
+testRule({
+  ruleName,
+  config: true,
+  skipBasicChecks: true,
+  codeFilename: `blocks/_modifier/${widthZeroName}_modifier.css`,
+  accept: [
+    {
+      code: `.${widthZeroName}_modifier { width: 1px; }`,
+      description: `blocks/_modifier/${widthZeroName}_modifier.css`,
+    },
   ],
   reject: [
-    // {
-    //   code: `
-    //     .page { width: 0; }
-    //     @media (max-width: 800px) { .page { width: 0; } }`,
-    //   message: messages.unexpectedDuplicatedPropertyValue('width: 0', '.page', '@media (max-width: 800px)'),
-    // },
+    {
+      code: `.${widthZeroName}_modifier { width: 0; }`,
+      message: messages.unexpectedDuplicatedPropertyValue('width: 0', `.${widthZeroName}_modifier`, `.${widthZeroName}`),
+      description: `blocks/_modifier/${widthZeroName}_modifier.css`,
+    },
   ],
 });
 
