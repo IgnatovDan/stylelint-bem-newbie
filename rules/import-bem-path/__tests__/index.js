de3cac0b61613f0e07ae226977ac29a33aa50c4f
Ignatov Dan <ignatovdan@gmail.com>
2023-02-02 22:23:51 +0300

    Change import-bem-path

--- rules/import-bem-path/__tests__/index.js
+++ rules/import-bem-path/__tests__/index.js
@@ -1,8 +1,8 @@
 const getTestRule = require('jest-preset-stylelint/getTestRule');
 
-const testRule = getTestRule({ plugins: ['./rules/import-bem-path.js'] });
+const testRule = getTestRule({ plugins: ['./rules/import-bem-path'] });
 
-const { ruleName, rule } = require('../rules/import-bem-path');
+const { ruleName, rule } = require('..');
 
 const { messages } = rule;
 
