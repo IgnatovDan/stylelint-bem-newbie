5c4ab56dd6fdf592d39e1da171a6dbe76bac2f5b
Ignatov Dan <ignatovdan@gmail.com>
2023-02-09 00:25:04 +0300

    Add rule

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const classNameEqualToFileName = require('./rules/class-name-equal-to-file-name'
 const duplicatedPropertyValueInMedia = require('./rules/duplicated-property-value-in-media');
 const duplicatedPropertyValueInModifier = require('./rules/duplicated-property-value-in-modifier');
 const absoluteHasTwoDimensions = require('./rules/absolute-has-two-dimensions');
+const animationExplicitTimingFunction = require('./rules/animation-explicit-timing-function');
+
 const importBemPath = require('./rules/import-bem-path');
 
 const plugins = [
@@ -17,6 +19,7 @@ const plugins = [
   duplicatedPropertyValueInMedia,
   duplicatedPropertyValueInModifier,
   absoluteHasTwoDimensions,
+  animationExplicitTimingFunction,
   importBemPath,
 ];
 
